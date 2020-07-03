b4f8a9f2dc1e807b4c01045ce3f7915428d05276
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2020-07-04 00:59:03 +0200

    App Started

---
+++ JS Task App/js/app.js
@@ -0,0 +1,42 @@
+//Select all the items
+const taskForm = document.querySelector('#tasks-form')
+let taskTitle = document.querySelector('#task-title')
+let taskDate = document.querySelector('#task-date')
+let taskDescription = document.querySelector('#task-description')
+const addTaskBtn = document.querySelector('#add-task-btn')
+const taskList = document.querySelector('.task-list')
+const deleteAllTaskBtn = document.querySelector('#delete-all-task-btn')
+const hideShowForm = document.querySelector('#hide-form-btn')
+//Save task data
+taskForm.addEventListener('submit',(e)=>{
+    e.preventDefault() //Cancel the default form event*/
+})
+addTaskBtn.addEventListener('click',()=>{
+    let task = {
+        taskTitle: taskTitle,
+        taskDescription: taskDescription,
+        taskDate: taskDate
+    }
+    taskList.innerHTML += `<div class ="task-container">
+        <h4>${task.taskTitle.value}</h4>
+        <p>${task.taskDescription.value}</p>
+        <p id="task-date-value">${task.taskDate.value}</p>
+        </div>`
+       /* let savedTitle = localStorage.setItem('Task Title',task.taskTitle.value)
+        let savedDate = localStorage.setItem('Task Date', task.taskDate.value)
+        let savedDescription = localStorage.setItem('Task Description',task.taskDescription.value)*/
+    taskForm.reset()
+})
+deleteAllTaskBtn.addEventListener('click',()=>{
+    taskList.innerHTML = ''
+})
+function HideShowForm(){
+    taskForm.classList.toggle('hide')
+    hideShowForm.classList.toggle('togglebtn')
+    taskList.classList.toggle('moveTask')
+    if(taskForm.classList == 'hide'){
+        hideShowForm.textContent = 'Show'
+    }else{
+        hideShowForm.textContent = 'Hide'
+    }
+}
