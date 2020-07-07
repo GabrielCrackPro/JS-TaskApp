52cfebfd92612b8e3e9c8e2f4f0e216dc59ed649
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2020-07-08 01:31:56 +0200

    Alerts Added

--- JS Task App/js/app.js
+++ JS Task App/js/app.js
@@ -1,18 +1,23 @@
 //Select all the items
 const taskForm = document.querySelector('#tasks-form')
-let taskTitle = document.querySelector('#task-title')
-let taskDate = document.querySelector('#task-date')
-let taskCategory = document.querySelector('#task-category')
-let taskDescription = document.querySelector('#task-description')
 const addTaskBtn = document.querySelector('#add-task-btn')
 const taskList = document.querySelector('.task-list')
 const deleteAllTaskBtn = document.querySelector('#delete-all-task-btn')
 const hideShowForm = document.querySelector('#hide-form-btn')
+const taskAddedAlert = document.querySelector('.task-added-alert')
+const taskRemovedAlert = document.querySelector('.task-removed-alert')
+const closeAddAlertBtn = document.querySelector('#close-task-added-alert')
+const closeRemovedAlertBtn = document.querySelector('#close-task-removed-alert')
+
+let taskTitle = document.querySelector('#task-title')
+let taskDate = document.querySelector('#task-date')
+let taskCategory = document.querySelector('#task-category')
+let taskDescription = document.querySelector('#task-description')
 //Save task data
-taskForm.addEventListener('submit',(e)=>{
+taskForm.addEventListener('submit', (e) => {
     e.preventDefault() //Cancel the default form event
 })
-addTaskBtn.addEventListener('click',()=>{
+addTaskBtn.addEventListener('click', () => {
     let task = {
         taskTitle: taskTitle,
         taskDescription: taskDescription,
@@ -20,25 +25,33 @@ addTaskBtn.addEventListener('click',()=>{
         taskCategory: taskCategory
     }
     taskList.innerHTML += `<div class ="task-container">
-        <h4>${task.taskTitle.value}</h4>
-        <p>${task.taskDescription.value}</p>
-        <p id="task-date-value">${task.taskDate.value}</p>
-        <p id="task-category-value">${task.taskCategory.value}</p>
-        <hr>
-        </div>`
+    <h4>${task.taskTitle.value}</h4>
+    <p>${task.taskDescription.value}</p>
+    <p id="task-date-value">${task.taskDate.value}</p>
+    <p id="task-category-value">${task.taskCategory.value}</p>
+    <hr>
+    </div>`
+    taskAddedAlert.style.display = 'block' //Show alert
     taskForm.reset()
 })
-deleteAllTaskBtn.addEventListener('click',()=>{
+deleteAllTaskBtn.addEventListener('click', () => {
     taskList.innerHTML = ''
+    taskRemovedAlert.style.display = 'block' //Show alert
 })
 //Animate form
-function HideShowForm(){
+function HideShowForm() {
     taskForm.classList.toggle('hide')
-    hideShowForm.classList.toggle('togglebtn')
     taskList.classList.toggle('moveTask')
-    if(taskForm.classList == 'hide'){
+    if (taskForm.classList == 'hide') {
         hideShowForm.textContent = 'Show'
-    }else{
+    } else {
         hideShowForm.textContent = 'Hide'
     }
 }
+//Animate alerts
+closeAddAlertBtn.addEventListener('click', () => {
+    taskAddedAlert.classList.add('alertClose')
+})
+closeRemovedAlertBtn.addEventListener('click', () => {
+    taskRemovedAlert.classList.add('alertClose')
+})
